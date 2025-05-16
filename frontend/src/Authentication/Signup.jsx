126bcb82a254d27f41c48ac85f9b2b125f9f5126
Ajay-kannan2006 <ajaykannan1001200635@gmail.com>
2025-05-16 12:17:18 +0530

    url update 2

--- frontend/src/Authentication/Signup.jsx
+++ frontend/src/Authentication/Signup.jsx
@@ -35,7 +35,7 @@ const Signup = () => {
 
     try {
       const res = await axios.post(
-        "https://unimeet-8ox2.onrender.comapi/auth/signup",
+        "https://unimeet-8ox2.onrender.com/api/auth/signup",
         formData,
         {
           withCredentials: true,
@@ -54,7 +54,7 @@ const Signup = () => {
     const checkUser = async () => {
       try {
         const res = await axios.get(
-          "https://unimeet-8ox2.onrender.comapi/auth/check-user",
+          "https://unimeet-8ox2.onrender.com/api/auth/check-user",
           {
             withCredentials: true,
           }
